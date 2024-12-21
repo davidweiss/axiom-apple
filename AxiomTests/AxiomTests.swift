53d50ac8240ac86b736226535cdc98abfcab1b04
David Weiss <davidweiss@users.noreply.github.com>
2024-12-20 19:02:43 -0700

    Initial project

---
+++ AxiomTests/AxiomTests.swift
@@ -0,0 +1,16 @@
+//
+//  AxiomTests.swift
+//  AxiomTests
+//
+//  Created by David Weiss on 12/20/24.
+//
+
+import Testing
+
+struct AxiomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
